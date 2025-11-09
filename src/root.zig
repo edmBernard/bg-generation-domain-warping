14ab80b418b733cd2e898e57488bcd1d9011dbd7
Erwan BERNARD <bernard.erwan@gmail.com>
2025-11-09 16:59:06 +0100

    feat: good2 with k3

--- src/root.zig
+++ src/root.zig
@@ -2,7 +2,7 @@
 const std = @import("std");
 pub const PatternType = @import("types").PatternType;
 
-const time = 126.0;
+const time = 123.0;
 
 const Vec2 = struct {
     x: f32,
@@ -22,10 +22,26 @@ const Vec3 = struct {
     }
 };
 
+inline fn normalize3(v: Vec3) Vec3 {
+    const len = std.math.sqrt(dot3(v, v));
+    return .{
+        .x = v.x / len,
+        .y = v.y / len,
+        .z = v.z / len,
+    };
+}
+
 inline fn mul(a: Vec2, b: f32) Vec2 {
     return .{
         .x = a.x * b,
         .y = a.y * b,
+    };
+}
+inline fn mul3(a: Vec3, b: f32) Vec3 {
+    return .{
+        .x = a.x * b,
+        .y = a.y * b,
+        .z = a.z * b,
     };
 }
 
@@ -55,6 +71,17 @@ inline fn lerp3(a: Vec3, b: Vec3, t: f32) Vec3 {
         .y = std.math.lerp(a.y, b.y, t),
         .z = std.math.lerp(a.z, b.z, t),
     };
+}
+
+// perform Hermite interpolation between two values
+inline fn smoothstep(edge0: f32, edge1: f32, x: f32) f32 {
+    const t = std.math.clamp((x - edge0) / (edge1 - edge0), 0.0, 1.0);
+    return t * t * (3.0 - 2.0 * t);
+}
+
+inline fn supersmoothstep(edge0: f32, edge1: f32, x: f32) f32 {
+    const t = std.math.clamp((x - edge0) / (edge1 - edge0), 0.0, 1.0);
+    return t * t * t * (10 - 15 * t + 6 * t * t);
 }
 
 inline fn forEach3(a: Vec3, fnc: fn (f32) f32) Vec3 {
@@ -110,7 +137,7 @@ fn noise(p: Vec2) f32 {
 }
 
 // rotation matrix to avoid direction artifacts
-const angle = std.math.pi / 3.0;
+const angle = std.math.pi / 4.0;
 const mtx = [4]f32{ @cos(angle), @sin(angle), -@sin(angle), @cos(angle) };
 
 // fbm noise implementation adapted from Inigo Quilez : https://iquilezles.org/articles/fbm/
@@ -121,14 +148,31 @@ fn fbm(comptime octaves: i32, vec: Vec2) f32 {
     // f = 1.0 f*= 2.01; nice
     // f = 2.0 f*= 1.01; nice
     var f: f32 = 1.0;
-    var a: f32 = 1.0;
+    var a: f32 = 0.5;
     var t: f32 = 0.0;
     for (0..octaves) |_| {
         t += a * noise(mul(matmul(mtx, vec), f));
-        f *= 2.01;
+        f *= 1.9;
         a *= G;
     }
     return t;
+}
+
+fn fbm6(vec: Vec2) f32 {
+    // H (Hurst exponent) determines the self similarity it recommand to use 0.5
+    const H = 1.0; // change a lot the visual aspect
+    const G = std.math.exp2(-H);
+    // f = 1.0 f*= 2.01; nice
+    // f = 2.0 f*= 1.01; nice
+    var f: f32 = 1.0;
+    var a: f32 = 0.5;
+    var t: f32 = 0.0;
+    for (0..6) |_| {
+        t += a * noise(mul(matmul(mtx, vec), f));
+        f *= 2.1;
+        a *= G;
+    }
+    return t * 1.1;
 }
 
 fn pattern1(p: Vec2) f32 {
@@ -148,20 +192,24 @@ fn pattern2(p: Vec2) struct { f32, Vec2 } {
 }
 
 fn pattern3(p: Vec2) struct { f32, Vec2, Vec2 } {
+    // low frequency
     const q: Vec2 = .{
-        .x = fbm(6, .{ .x = p.x + 0, .y = p.y + 0 }),
-        .y = fbm(6, .{ .x = p.x + 5.2, .y = p.y + 1.3 }),
+        .x = 0.5 + 0.5 * fbm(4, .{ .x = p.x + 1.0, .y = p.y + 0.1 }),
+        .y = 0.5 + 0.5 * fbm(4, .{ .x = p.x + 5.2, .y = p.y + 1.3 }),
     };
 
+    // mid frequency
     const r: Vec2 = .{
-        .x = fbm(4, .{ .x = p.x + 2.1 * q.x + 1.7, .y = p.y + 2.1 * q.y + 9.2 }),
-        .y = fbm(4, .{ .x = p.x + 2.1 * q.x + 8.3, .y = p.y + 2.1 * q.y + 2.8 }),
+        .x = 0.5 - 0.5 * fbm6(.{ .x = 4.1 * q.x, .y = 4.1 * q.y }),
+        .y = 0.5 - 0.5 * fbm6(.{ .x = 4.1 * q.x, .y = 4.1 * q.y }),
     };
 
-    return .{ fbm(4, .{
+    // high frequency
+    const f: f32 = 0.5 + 0.5 * fbm(4, .{
         .x = p.x + 2.1 * r.x,
         .y = p.y + 2.1 * r.y,
-    }), q, r };
+    });
+    return .{ f, r, q };
 }
 
 pub fn generate_image(allocator: std.mem.Allocator, width: u32, height: u32, pattern: PatternType) !std.ArrayList(u8) {
@@ -197,16 +245,46 @@ pub fn generate_image(allocator: std.mem.Allocator, width: u32, height: u32, pat
                     break :blk .{ col.x, col.y, col.z };
                 },
                 PatternType.k3 => blk: {
-                    const f, const q, const r = pattern3(.{ .x = x, .y = y });
-                    var col = Vec3.fromHexWithAlpha(0x333facff); // #333facff
+                    const f, const r, const q = pattern3(.{ .x = x, .y = y });
+                    var col = Vec3.fromHexWithAlpha(0x561111ff); // #561111ff
 
-                    col = lerp3(col, Vec3.fromHexWithAlpha(0x000000ff), f); // #000000ff
-                    col = lerp3(col, Vec3.fromHexWithAlpha(0x1ca1e9ff), dot2(r, r)); // #1ca1e9ff
-                    col = lerp3(col, Vec3.fromHexWithAlpha(0x0adaffff), dot2(q, q)); // #0adaffff
+                    // _ = f;
+                    // _ = r;
+                    // _ = q;
+                    col = lerp3(col, Vec3.fromHexWithAlpha(0x490101ff), f); // #490101ff
+                    col = lerp3(col, Vec3.fromHexWithAlpha(0xffffffff), dot2(r, r)); // #ffffffff
+                    col = lerp3(col, Vec3.fromHexWithAlpha(0x832121ff), dot2(q, q)); // #832121ff
+                    // col = lerp3(col, Vec3.fromHexWithAlpha(0x0adaffff), 0.5 * q.y * q.y); // #0adaffff
+                    col = lerp3(
+                        col,
+                        Vec3.fromHexWithAlpha(0x021b29ff), // #021b29ff
+                        0.5 * smoothstep(1.1, 1.3, @abs(r.x) + @abs(r.y)),
+                    );
 
+                    const e = 1.0 / scale;
+
+                    const fex, _, _ = pattern3(.{ .x = x + e, .y = y });
+                    const fey, _, _ = pattern3(.{ .x = x, .y = y + e });
+
+                    // nor.x is the derivative of pattern3 along x
+                    // nor.y is the step
+                    // nor.z is the derivative of pattern3 along y
+                    const nor = normalize3(.{ .x = fex - f, .y = e, .z = fey - f });
+
+                    const lig = normalize3(.{ .x = 0.9, .y = -0.2, .z = -0.4 });
+                    const diff = std.math.clamp(0.3 + 0.7 * dot3(nor, lig), 0.0, 1.0);
+
+                    const lin: Vec3 = .{
+                        .x = 0.85 * (nor.y * 0.5 + 0.5) + 0.15 * diff,
+                        .y = 0.90 * (nor.y * 0.5 + 0.5) + 0.10 * diff,
+                        .z = 0.95 * (nor.y * 0.5 + 0.5) + 0.05 * diff,
+                    };
+                    col = .{ .x = col.x * lin.x, .y = col.y * lin.y, .z = col.z * lin.z };
+
+                    col = mul3(col, f * 2.0);
                     const functor = struct {
                         pub fn call(value: f32) f32 {
-                            return value * value;
+                            return std.math.pow(f32, 1 - value, 2);
                         }
                     }.call;
                     col = forEach3(col, functor);
